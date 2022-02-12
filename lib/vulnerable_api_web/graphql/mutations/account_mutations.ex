e696951833452479b5f4d208e88ab11a7c2613a6
dyey <jun@mashupgarage.com>
2022-02-12 19:28:05 +0800

    Adds accounts mutations

--- lib/vulnerable_api_web/graphql/mutations/account_mutations.ex
+++ lib/vulnerable_api_web/graphql/mutations/account_mutations.ex
@@ -16,5 +16,38 @@ defmodule VulnerableApiWeb.GraphQL.Mutations.AccountMutations do
 
       resolve(&AccountResolver.create_user/2)
     end
+
+    @desc "Log In"
+    field :login, :user do
+      arg :email, non_null(:string)
+      arg :password, non_null(:string)
+
+      resolve(&AccountResolver.login/2)
+    end
+
+    @desc "Update Profile"
+    field :update_profile, :user do
+      arg :user_id, :string
+      arg :email, non_null(:string)
+      arg :full_name, non_null(:string)
+      arg :address, non_null(:string)
+
+      resolve(&AccountResolver.update_profile/2)
+    end
+
+    @desc "Delete User"
+    field :delete_user, :user do
+      arg :user_id, non_null(:string)
+
+      resolve(&AccountResolver.delete_user/2)
+    end
+
+    @desc "Add Credits"
+    field :add_credits, :user do
+      arg :user_id, non_null(:string)
+      arg :credits, non_null(:integer)
+
+      resolve(&AccountResolver.update_user/2)
+    end
   end
 end
--- lib/vulnerable_api_web/graphql/resolvers/account_resolver.ex
+++ lib/vulnerable_api_web/graphql/resolvers/account_resolver.ex
@@ -1,11 +1,35 @@
 defmodule VulnerableApiWeb.GraphQL.Resolvers.AccountResolver do
   alias VulnerableApi.Accounts
 
+  def login(args, _) do
+    args
+  end
+
   def get_user(%{user_id: user_id}, _) do
     {:ok, Accounts.get_user(user_id)}
   end
 
   def create_user(args, _) do
     Accounts.create_user(args)
+  end
+
+  def update_user(%{user_id: user_id} = args, _) do
+    case Accounts.get_user(user_id) do
+      %{id: _} = user ->
+        Accounts.update_user(user, args)
+
+      _ ->
+        {:error, %{message: "User Not found."}}
+    end
+  end
+
+  def delete_user(%{user_id: user_id}, _) do
+    case Accounts.get_user(user_id) do
+      %{id: _} = user ->
+        {:ok, Accounts.delete_user(user)}
+
+      _ ->
+        {:error, %{message: "User Not found."}}
+    end
   end
 end
