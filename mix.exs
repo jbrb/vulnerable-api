ddac3af0f229d7e55ac13aab65cd30de80841bd6
jbrb <jbr.benin@gmail.com>
2022-02-12 18:09:06 +0800

    added guardian and bcrypt

--- mix.exs
+++ mix.exs
@@ -45,7 +45,9 @@ defmodule VulnerableApi.MixProject do
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
       {:absinthe, "~> 1.6"},
-      {:absinthe_plug, "~> 1.5"}
+      {:absinthe_plug, "~> 1.5"},
+      {:guardian, "~> 2.2"},
+      {:bcrypt_elixir, "~> 3.0"}
     ]
   end
 
